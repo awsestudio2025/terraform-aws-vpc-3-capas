1beeb8070e6c61a2d0401c7ade7b957112e6916a
aswestudio2025 <awsestudio2025@gmail.com>
2025-10-03 21:57:24 -0500

    separacion de archivos2

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     bucket         = "awsestudio2025-terraform-state" # Reemplazar con el nombre de tu bucket S3 creado manualmente
     key            = "vpc-project/terraform.tfstate"
-    region         = var.aws_region
+    region         = "us-east-1"
     encrypt        = true
   }
 }
